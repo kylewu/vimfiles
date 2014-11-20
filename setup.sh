20295f613f0b80f53e6baba08d3444b3b5ca215a
Wenbin Wu <wenbin.wu@schibsted.es>
2014-11-20 12:02:30 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Downloading Vundle"
+git submodule init
+git submodule update
+
+ROOT=`pwd`
+
 echo "Linking vim files"
 
-ln -s "($pwd)/.vim" ~/
-ln -s "($pwd)/.vimrc" ~/
+ln -s "$ROOT/.vim" ~
+ln -s "$ROOT/.vimrc" ~
 
 echo "Installing bundles"
 vim +BundleInstall +qall
